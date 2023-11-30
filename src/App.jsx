f9ab005054ce0fb5fe5fdc709a062f1fd241ddce
yutakikuchi51 <120287555+yutakikuchi51@users.noreply.github.com>
2023-11-29 22:36:23 -0700

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import AddExpenseScreen from './AddExpenseScreen'; 
-import BudgetScreen from './BudgetScreen'; 
-import StatsScreen from './StatsScreen';
+import AddExpenseScreen from './screens/AddExpenseScreen'; 
+import BudgetScreen from './screens/BudgetScreen'; 
+import StatsScreen from './screens/StatsScreen';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AddExpense">
+      <Stack.Navigator initialRouteName="Budget">
         <Stack.Screen name="AddExpense" component={AddExpenseScreen} />
         <Stack.Screen name="Budget" component={BudgetScreen} />
         <Stack.Screen name="Stats" component={StatsScreen} />
