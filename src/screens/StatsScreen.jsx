db118eecaa6b16d2bc6b3c37d99fc4131d2f967f
DONGYUN KIM <kimdy960124@gmail.com>
2023-12-01 14:40:02 -0700

    Update StatsScreen.jsx
    Previous & Next months are available to see

--- src/screens/StatsScreen.jsx
+++ src/screens/StatsScreen.jsx
@@ -1,21 +1,66 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableHighlight  } from 'react-native';
 import TransactionType from '../components/TransactionType';
 
 function StatsScreen() {
     const [type, setType] = useState('Expense');
-    const startDate = '2023/10/01';
-    const endDate = '2023/10/30';
+    const [startDate, setStartDate] = useState('2023/10/01');
+    const [endDate, setEndDate] = useState('2023/10/30');
+
+    const handlePreviousMonth = () => {
+        const [year, month, day] = startDate.split('/');
+        const currentMonth = parseInt(month);
+        let newYear = parseInt(year);
+        let newMonth = currentMonth - 1;
+
+        if (newMonth === 0) {
+            newMonth = 12;
+            newYear--;
+        }
+
+        const previousStartDate = `${newYear}/${newMonth.toString().padStart(2, '0')}/01`;
+        setStartDate(previousStartDate);
+
+        const daysInPreviousMonth = new Date(newYear, newMonth, 0).getDate();
+        const previousEndDate = `${newYear}/${newMonth.toString().padStart(2, '0')}/${daysInPreviousMonth}`;
+        setEndDate(previousEndDate);
+    };
+
+    const handleNextMonth = () => {
+        const [year, month, day] = startDate.split('/');
+        const currentMonth = parseInt(month);
+        let newYear = parseInt(year);
+        let newMonth = currentMonth + 1;
+
+        if (newMonth === 13) {
+            newMonth = 1; // Set to January
+            newYear++;
+        }
+
+        const nextStartDate = `${newYear}/${newMonth.toString().padStart(2, '0')}/01`;
+        setStartDate(nextStartDate);
+
+        const daysInNextMonth = new Date(newYear, newMonth, 0).getDate();
+        const nextEndDate = `${newYear}/${newMonth.toString().padStart(2, '0')}/${daysInNextMonth}`;
+        setEndDate(nextEndDate);
+    };
 
     return (
         <ScrollView style={styles.container}>
             <View style={styles.datePeriodContainer}>
-                <Text style={styles.datePeriodText}>Date Period: {startDate} - {endDate}</Text>
+                <TouchableHighlight onPress={handlePreviousMonth}>
+                    <Text style={styles.arrow}>{'<'}</Text>
+                </TouchableHighlight>
+                <Text style={styles.datePeriodText}>{startDate} - {endDate}</Text>
+                <TouchableHighlight onPress={handleNextMonth}>
+                    <Text style={styles.arrow}>{'>'}</Text>
+                </TouchableHighlight>
             </View>
             <TransactionType type={type} onSelect={setType} />
         </ScrollView>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -23,6 +68,9 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     datePeriodContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         backgroundColor: '#f0f0f0',
         padding: 10,
         borderRadius: 8,
@@ -30,6 +78,10 @@ const styles = StyleSheet.create({
     },
     datePeriodText: {
         fontSize: 16,
+        fontWeight: 'bold',
+    },
+    arrow: {
+        fontSize: 20,
         fontWeight: 'bold',
     },
 });
