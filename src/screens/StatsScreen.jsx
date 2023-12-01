b39935ea96835b219be6c0d8ddab34a95fdd9c72
DONGYUN KIM <kimdy960124@gmail.com>
2023-12-01 14:19:15 -0700

    Edited StatsScreen.jsx
    added date period on StatsScreen.jsx

--- src/screens/StatsScreen.jsx
+++ src/screens/StatsScreen.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import TransactionType from '../components/TransactionType';
 
 function StatsScreen() {
     const [type, setType] = useState('Expense');
+    const startDate = '2023/10/01';
+    const endDate = '2023/10/30';
 
     return (
         <ScrollView style={styles.container}>
+            <View style={styles.datePeriodContainer}>
+                <Text style={styles.datePeriodText}>Date Period: {startDate} - {endDate}</Text>
+            </View>
             <TransactionType type={type} onSelect={setType} />
         </ScrollView>
     );
@@ -17,6 +22,16 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
     },
-  });
+    datePeriodContainer: {
+        backgroundColor: '#f0f0f0',
+        padding: 10,
+        borderRadius: 8,
+        marginBottom: 20,
+    },
+    datePeriodText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
 
 export default StatsScreen;
