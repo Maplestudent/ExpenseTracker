3e4844f361f3c85be7bbaa449e425b3145b45ece
monkeyV10 <119895558+monkeyV10@users.noreply.github.com>
2023-12-01 19:22:08 -0700

    transaction type categories

--- src/screens/AddExpenseScreen.jsx
+++ src/screens/AddExpenseScreen.jsx
@@ -4,56 +4,60 @@ import AddNotes from '../components/AddNotes';
 import InputAmount from '../components/InputAmount';
 import SelectCategory from '../components/SelectCategory';
 import SelectDate from '../components/SelectDate';
-import { StorageContext } from '../components/Storage'; // Import the StorageContext
+import { StorageContext } from '../components/Storage';
 import TransactionType from '../components/TransactionType';
 
+const expenseCategories = [
+  { label: 'Food', value: 'Food' },
+  { label: 'Transport', value: 'Transport' },
+  { label: 'Utilities', value: 'Utilities' },
+  { label: 'Entertainment', value: 'Entertainment' },
+  { label: 'Healthcare', value: 'Healthcare' },
+];
 
+const incomeCategories = [
+  { label: 'Salary', value: 'Salary' },
+  { label: 'Gifts', value: 'Gifts' },
+  { label: 'Investments', value: 'Investments' },
+  { label: 'Other', value: 'Other' },
+];
 
-
-
-function AddExpenseScreen({ navigation }) {  // Added navigation prop here
+function AddExpenseScreen({ navigation }) {
   const [amount, setAmount] = useState('');
   const [type, setType] = useState('Expense');
   const [category, setCategory] = useState('Food');
   const [date, setDate] = useState(new Date());
   const [note, setNote] = useState('');
-  const categories = [
-    { label: 'Food', value: 'Food' },
-    { label: 'Transport', value: 'Transport' },
-    { label: 'Utilities', value: 'Utilities' },
-    { label: 'Entertainment', value: 'Entertainment' },
-    { label: 'Healthcare', value: 'Healthcare' }
-  ];
+  const [categories, setCategories] = useState(expenseCategories); // State for categories based on type
 
-  const { addExpense } = useContext(StorageContext); // Use the context
+  const { addExpense } = useContext(StorageContext);
+
+  // Update categories when transaction type changes
+  const handleTypeSelect = (selectedType) => {
+    setType(selectedType);
+    const newCategories = selectedType === 'Expense' ? expenseCategories : incomeCategories;
+    setCategories(newCategories);
+    setCategory(newCategories[0].value); // Reset category when type changes
+  };
 
   const handleAddExpense = () => {
     const newExpense = { amount, type, category, date: date.toISOString(), note };
-    addExpense(newExpense); // Save the expense
-    navigation.navigate('Stats'); // Navigate to Stats screen
+    addExpense(newExpense);
+    navigation.navigate('Stats');
   };
 
-    return (
-      <ScrollView style={styles.container}>
-          <Text style={styles.header}>Add Expense</Text>
-          <InputAmount value={amount} onChange={setAmount} />
-          <TransactionType type={type} onSelect={setType} />
-          <SelectCategory selectedValue={category} onValueChange={setCategory} categories={categories} />
-          <SelectDate date={date} onDateChange={setDate} />
-          <AddNotes value={note} onChange={setNote} />
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Add Expense</Text>
+      <InputAmount value={amount} onChange={setAmount} />
+      <TransactionType type={type} onSelect={handleTypeSelect} />
+      <SelectCategory selectedValue={category} onValueChange={setCategory} categories={categories} />
+      <SelectDate date={date} onDateChange={setDate} />
+      <AddNotes value={note} onChange={setNote} />
 
-      <Button
-        title="Save Expense"
-        onPress={handleAddExpense} // Use handleAddExpense
-      />
-      <Button
-        title="View Stats"
-        onPress={() => navigation.navigate('Stats')} // Navigate to StatsScreen
-      />
-      <Button
-        title="Go to Budget"
-        onPress={() => navigation.navigate('Budget')}
-      />
+      <Button title="Save Expense" onPress={handleAddExpense} />
+      <Button title="View Stats" onPress={() => navigation.navigate('Stats')} />
+      <Button title="Go to Budget" onPress={() => navigation.navigate('Budget')} />
     </ScrollView>
   );
 }
@@ -68,7 +72,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-
+  // Add styles for your components as needed
 });
 
 export default AddExpenseScreen;
